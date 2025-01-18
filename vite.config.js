428e74cc4c7b892c160d2ff56010ea66ac7e7a90
Neepurna <neepurna@gmail.com>
2025-01-18 14:47:26 -0500

    updated materialUI in pacakge.json and vite.config.s for netlify server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@mui/material',
+        '@emotion/react',
+        '@emotion/styled',
+        '@mui/icons-material'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@mui/material': '@mui/material/index',
+      '@mui/icons-material': '@mui/icons-material/index'
+    }
+  }
 })
